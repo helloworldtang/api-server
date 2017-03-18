d10e2acd0bd142d2ccde7bbc353f361830c48fed
tang.cheng <tang.cheng@xiaoyi.com>
2017-03-18 17:36:42 +0800

    Initial commit httpsession demo

---
+++ httpsession/src/main/java/com/api/HttpSessionApplication.java
@@ -0,0 +1,12 @@
+package com.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpSessionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HttpSessionApplication.class, args);
+    }
+}
