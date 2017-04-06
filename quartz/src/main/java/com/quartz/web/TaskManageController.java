812fe083900c5cbd3572af515a5ec712d0c5784f
tang.cheng <tang.cheng@xiaoyi.com>
2017-04-06 12:07:48 +0800

    add comment

--- quartz/src/main/java/com/quartz/web/TaskManageController.java
+++ quartz/src/main/java/com/quartz/web/TaskManageController.java
@@ -18,105 +18,125 @@ import java.util.Map;
 
 /**
  * 任务管理
+ *
  * @author lance
  */
 @Controller
 public class TaskManageController {
-	@Autowired
-	private TaskServiceImpl taskServiceImpl;
+    @Autowired
+    private TaskServiceImpl taskServiceImpl;
 
-	/**
-	 * Index.jsp
-	 * 2016年10月8日下午6:39:15
-	 */
-	@RequestMapping(value={"", "/", "index"})
-	public String info(){
-		return "index";
-	}
-	
-	/**
-	 * 任务列表
-	 * @return
-	 * 2016年10月9日上午11:36:03
-	 */
-	@ResponseBody
-	@RequestMapping(value="list", method=RequestMethod.POST)
-	public String list(){
-		Map<String, Object> map = new HashMap<>();
-		List<TaskInfo> infos = taskServiceImpl.list();
-		map.put("rows", infos);
-		map.put("total", infos.size());
-		return JSON.toJSONString(map);
-	}
-	
-	/**
-	 * 保存定时任务
-	 * @param info
-	 * 2016年10月9日下午1:36:59
-	 */
-	@ResponseBody
-	@RequestMapping(value="save", method=RequestMethod.POST, produces = "application/json; charset=UTF-8")
-	public String save(TaskInfo info){
-		try {
-			if(info.getId() == 0) {
-				taskServiceImpl.addJob(info);
-			}else{
-				taskServiceImpl.edit(info);
-			}
-		} catch (ServiceException e) {
-			return ResultInfo.error(-1, e.getMessage());
-		}
-		return ResultInfo.success();
-	}
-	
-	/**
-	 * 删除定时任务
-	 * @param jobName
-	 * @param jobGroup
-	 * 2016年10月9日下午1:52:20
-	 */
-	@ResponseBody
-	@RequestMapping(value="delete/{jobName}/{jobGroup}", produces = "application/json; charset=UTF-8")
-	public String delete(@PathVariable String jobName, @PathVariable String jobGroup){
-		try {
-			taskServiceImpl.delete(jobName, jobGroup);
-		} catch (ServiceException e) {
-			return ResultInfo.error(-1, e.getMessage());
-		}
-		return ResultInfo.success();
-	}
-	
-	/**
-	 * 暂停定时任务
-	 * @param jobName
-	 * @param jobGroup
-	 * 2016年10月10日上午9:41:25
-	 */
-	@ResponseBody
-	@RequestMapping(value="pause/{jobName}/{jobGroup}", produces = "application/json; charset=UTF-8")
-	public String pause(@PathVariable String jobName, @PathVariable String jobGroup){
-		try {
-			taskServiceImpl.pause(jobName, jobGroup);
-		} catch (ServiceException e) {
-			return ResultInfo.error(-1, e.getMessage());
-		}
-		return ResultInfo.success();
-	}
-	
-	/**
-	 * 重新开始定时任务
-	 * @param jobName
-	 * @param jobGroup
-	 * 2016年10月10日上午9:41:40
-	 */
-	@ResponseBody
-	@RequestMapping(value="resume/{jobName}/{jobGroup}", produces = "application/json; charset=UTF-8")
-	public String resume(@PathVariable String jobName, @PathVariable String jobGroup){
-		try {
-			taskServiceImpl.resume(jobName, jobGroup);
-		} catch (ServiceException e) {
-			return ResultInfo.error(-1, e.getMessage());
-		}
-		return ResultInfo.success();
-	}
+    /**
+     * Index.jsp
+     * 2016年10月8日下午6:39:15
+     */
+    @RequestMapping(value = {"", "/", "index"})
+    public String info() {
+        return "index";
+    }
+
+    /**
+     * 任务列表
+     *
+     * @return 2016年10月9日上午11:36:03
+     */
+    @ResponseBody
+    @RequestMapping(value = "list", method = RequestMethod.POST)
+    public String list() {
+        Map<String, Object> map = new HashMap<>();
+        List<TaskInfo> infos = taskServiceImpl.list();
+        map.put("rows", infos);
+        map.put("total", infos.size());
+        return JSON.toJSONString(map);
+    }
+
+    /**
+     * 保存定时任务
+     *
+     * @param info 2016年10月9日下午1:36:59
+     */
+    @ResponseBody
+    @RequestMapping(value = "save", method = RequestMethod.POST, produces = "application/json; charset=UTF-8")
+    public String save(TaskInfo info) {
+        try {
+            if (info.getId() == 0) {
+                taskServiceImpl.addJob(info);
+            } else {
+                taskServiceImpl.edit(info);
+            }
+        } catch (ServiceException e) {
+            return ResultInfo.error(-1, e.getMessage());
+        }
+        return ResultInfo.success();
+    }
+
+    /**
+     * 删除定时任务
+     *
+     * @param jobName
+     * @param jobGroup 2016年10月9日下午1:52:20
+     */
+    @ResponseBody
+    @RequestMapping(value = "delete/{jobName}/{jobGroup}", produces = "application/json; charset=UTF-8")
+    public String delete(@PathVariable String jobName, @PathVariable String jobGroup) {
+        try {
+            taskServiceImpl.delete(jobName, jobGroup);
+        } catch (ServiceException e) {
+            return ResultInfo.error(-1, e.getMessage());
+        }
+        return ResultInfo.success();
+    }
+
+    /**
+     * 暂停定时任务.暂停后再重新启动。暂停期间的执行次数，都会再补上的
+     * 2017-04-06 10:12:14.021  INFO 7660 --- [eduler_Worker-2] com.quartz.common.job.Minute3Job         : JobName3: com.quartz.common.job.Minute3Job
+     * 2017-04-06 10:12:16.058  INFO 7660 --- [eduler_Worker-3] com.quartz.common.job.Minute3Job         : JobName3: com.quartz.common.job.Minute3Job
+     * 2017-04-06 10:12:18.015  INFO 7660 --- [eduler_Worker-4] com.quartz.common.job.Minute3Job         : JobName3: com.quartz.common.job.Minute3Job
+     * 2017-04-06 10:12:19.744  INFO 7660 --- [p-nio-80-exec-4] com.quartz.service.TaskServiceImpl       : ===> Pause success, triggerKey:Job_group.com.quartz.common.job.Minute3Job
+     * 2017-04-06 10:12:37.853  INFO 7660 --- [p-nio-80-exec-6] com.quartz.service.TaskServiceImpl       : ===> Resume success, triggerKey:Job_group.com.quartz.common.job.Minute3Job
+     * 2017-04-06 10:12:37.920  INFO 7660 --- [eduler_Worker-5] com.quartz.common.job.Minute3Job         : JobName3: com.quartz.common.job.Minute3Job
+     * 2017-04-06 10:12:38.032  INFO 7660 --- [eduler_Worker-1] com.quartz.common.job.Minute3Job         : JobName3: com.quartz.common.job.Minute3Job
+     * 2017-04-06 10:12:38.078  INFO 7660 --- [eduler_Worker-2] com.quartz.common.job.Minute3Job         : JobName3: com.quartz.common.job.Minute3Job
+     * 2017-04-06 10:12:38.118  INFO 7660 --- [eduler_Worker-3] com.quartz.common.job.Minute3Job         : JobName3: com.quartz.common.job.Minute3Job
+     * 2017-04-06 10:12:38.156  INFO 7660 --- [eduler_Worker-4] com.quartz.common.job.Minute3Job         : JobName3: com.quartz.common.job.Minute3Job
+     * 2017-04-06 10:12:38.288  INFO 7660 --- [eduler_Worker-5] com.quartz.common.job.Minute3Job         : JobName3: com.quartz.common.job.Minute3Job
+     * 2017-04-06 10:12:38.526  INFO 7660 --- [eduler_Worker-1] com.quartz.common.job.Minute3Job         : JobName3: com.quartz.common.job.Minute3Job
+     * 2017-04-06 10:12:38.664  INFO 7660 --- [eduler_Worker-2] com.quartz.common.job.Minute3Job         : JobName3: com.quartz.common.job.Minute3Job
+     * 2017-04-06 10:12:38.810  INFO 7660 --- [eduler_Worker-3] com.quartz.common.job.Minute3Job         : JobName3: com.quartz.common.job.Minute3Job
+     * 2017-04-06 10:12:39.010  INFO 7660 --- [eduler_Worker-4] com.quartz.common.job.Minute3Job         : JobName3: com.quartz.common.job.Minute3Job
+     * 2017-04-06 10:12:40.058  INFO 7660 --- [eduler_Worker-5] com.quartz.common.job.Minute3Job         : JobName3: com.quartz.common.job.Minute3Job
+     * 2017-04-06 10:12:42.115  INFO 7660 --- [eduler_Worker-1] com.quartz.common.job.Minute3Job         : JobName3: com.quartz.common.job.Minute3Job
+     * 2017-04-06 10:12:44.032  INFO 7660 --- [eduler_Worker-2] com.quartz.common.job.Minute3Job         : JobName3: com.quartz.common.job.Minute3Job
+     * 2017-04-06 10:12:46.052  INFO 7660 --- [eduler_Worker-3] com.quartz.common.job.Minute3Job         : JobName3: com.quartz.common.job.Minute3Job
+     *
+     * @param jobName
+     * @param jobGroup 2016年10月10日上午9:41:25
+     */
+    @ResponseBody
+    @RequestMapping(value = "pause/{jobName}/{jobGroup}", produces = "application/json; charset=UTF-8")
+    public String pause(@PathVariable String jobName, @PathVariable String jobGroup) {
+        try {
+            taskServiceImpl.pause(jobName, jobGroup);
+        } catch (ServiceException e) {
+            return ResultInfo.error(-1, e.getMessage());
+        }
+        return ResultInfo.success();
+    }
+
+    /**
+     * 重新开始定时任务
+     *
+     * @param jobName
+     * @param jobGroup 2016年10月10日上午9:41:40
+     */
+    @ResponseBody
+    @RequestMapping(value = "resume/{jobName}/{jobGroup}", produces = "application/json; charset=UTF-8")
+    public String resume(@PathVariable String jobName, @PathVariable String jobGroup) {
+        try {
+            taskServiceImpl.resume(jobName, jobGroup);
+        } catch (ServiceException e) {
+            return ResultInfo.error(-1, e.getMessage());
+        }
+        return ResultInfo.success();
+    }
 }
--- quartz/src/main/resources/demo-schema.sql
+++ quartz/src/main/resources/demo-schema.sql
@@ -1,50 +1,20 @@
--- 表的结构 `qrtz_blob_triggers`
---
-
-CREATE TABLE IF NOT EXISTS `qrtz_blob_triggers` (
-  `SCHED_NAME` varchar(120) NOT NULL,
-  `TRIGGER_NAME` varchar(120) NOT NULL,
-  `TRIGGER_GROUP` varchar(120) NOT NULL,
-  `BLOB_DATA` blob
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
-
+-- 		SchedulerFactory schedulerFactory = new StdSchedulerFactory(quartzProperties());
+-- 		Scheduler scheduler = schedulerFactory.getScheduler();
+-- 		scheduler.start();  启动Scheduler时会查询表qrtz_locks
 --
 -- 表的结构 `qrtz_locks`
 --
-
 CREATE TABLE IF NOT EXISTS `qrtz_locks` (
   `SCHED_NAME` varchar(120) NOT NULL,
   `LOCK_NAME` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- 表的结构 `qrtz_fired_triggers`
---
 
-CREATE TABLE IF NOT EXISTS `qrtz_fired_triggers` (
-  `SCHED_NAME` varchar(120) NOT NULL,
-  `ENTRY_ID` varchar(95) NOT NULL,
-  `TRIGGER_NAME` varchar(120) NOT NULL,
-  `TRIGGER_GROUP` varchar(120) NOT NULL,
-  `INSTANCE_NAME` varchar(120) NOT NULL,
-  `FIRED_TIME` bigint(13) NOT NULL,
-  `SCHED_TIME` bigint(13) NOT NULL,
-  `PRIORITY` int(11) NOT NULL,
-  `STATE` varchar(16) NOT NULL,
-  `JOB_NAME` varchar(120) DEFAULT NULL,
-  `JOB_GROUP` varchar(120) DEFAULT NULL,
-  `IS_NONCONCURRENT` varchar(1) DEFAULT NULL,
-  `REQUESTS_RECOVERY` varchar(1) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-
+-- at org.quartz.impl.jdbcjobstore.StdJDBCDelegate.countMisfiredTriggersInState(StdJDBCDelegate.java:393) ~[quartz-2.2.3.jar:na]
+-- 获取misfires信息时，需要查表qrtz_triggers
 --
 -- 表的结构 `qrtz_triggers`
 --
-
 CREATE TABLE IF NOT EXISTS `qrtz_triggers` (
   `SCHED_NAME` varchar(120) NOT NULL,
   `TRIGGER_NAME` varchar(120) NOT NULL,
@@ -64,11 +34,13 @@ CREATE TABLE IF NOT EXISTS `qrtz_triggers` (
   `JOB_DATA` blob
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-
+-- org.quartz.JobPersistenceException: Failure identifying failed instances when checking-in
+-- 		SchedulerFactory schedulerFactory = new StdSchedulerFactory(quartzProperties());
+-- 		Scheduler scheduler = schedulerFactory.getScheduler();
+-- 		scheduler.start();//start时需要表qrtz_scheduler_state
 --
 -- 表的结构 `qrtz_scheduler_state`
 --
-
 CREATE TABLE IF NOT EXISTS `qrtz_scheduler_state` (
   `SCHED_NAME` varchar(120) NOT NULL,
   `INSTANCE_NAME` varchar(120) NOT NULL,
@@ -77,12 +49,46 @@ CREATE TABLE IF NOT EXISTS `qrtz_scheduler_state` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
---scheduler.getJobGroupNames()会查表qrtz_job_details
+-- org.quartz.JobPersistenceException: Failure identifying failed instances when checking-in:
+--Scheduler start时会使用
+-- 表的结构 `qrtz_fired_triggers`
+--
+CREATE TABLE IF NOT EXISTS `qrtz_fired_triggers` (
+  `SCHED_NAME` varchar(120) NOT NULL,
+  `ENTRY_ID` varchar(95) NOT NULL,
+  `TRIGGER_NAME` varchar(120) NOT NULL,
+  `TRIGGER_GROUP` varchar(120) NOT NULL,
+  `INSTANCE_NAME` varchar(120) NOT NULL,
+  `FIRED_TIME` bigint(13) NOT NULL,
+  `SCHED_TIME` bigint(13) NOT NULL,
+  `PRIORITY` int(11) NOT NULL,
+  `STATE` varchar(16) NOT NULL,
+  `JOB_NAME` varchar(120) DEFAULT NULL,
+  `JOB_GROUP` varchar(120) DEFAULT NULL,
+  `IS_NONCONCURRENT` varchar(1) DEFAULT NULL,
+  `REQUESTS_RECOVERY` varchar(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+---------------------有上面的表，Scheduler就可以start了。系统正常启动---------------------
+
+-- 表的结构 `qrtz_blob_triggers`
+--
+CREATE TABLE IF NOT EXISTS `qrtz_blob_triggers` (
+  `SCHED_NAME` varchar(120) NOT NULL,
+  `TRIGGER_NAME` varchar(120) NOT NULL,
+  `TRIGGER_GROUP` varchar(120) NOT NULL,
+  `BLOB_DATA` blob
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+
+
+-- org.quartz.JobPersistenceException: Couldn't obtain job groups:
+--scheduler.getJobGroupNames()会查表qrtz_job_details
 --
 -- 表的结构 `qrtz_job_details`
 --
-
 CREATE TABLE IF NOT EXISTS `qrtz_job_details` (
   `SCHED_NAME` varchar(120) NOT NULL,
   `JOB_NAME` varchar(120) NOT NULL,
@@ -97,25 +103,21 @@ CREATE TABLE IF NOT EXISTS `qrtz_job_details` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
---			scheduler.scheduleJob(jobDetail, trigger);
---- Couldn't store trigger
-
+--scheduler.scheduleJob(jobDetail, trigger);
+--org.quartz.JobPersistenceException: Couldn't store trigger
 --
 -- 表的结构 `qrtz_paused_trigger_grps`
 --
-
 CREATE TABLE IF NOT EXISTS `qrtz_paused_trigger_grps` (
   `SCHED_NAME` varchar(120) NOT NULL,
   `TRIGGER_GROUP` varchar(120) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---- Couldn't store trigger
---job:Table 'qrtz_cron_triggers' doesn't exist
-
+--org.quartz.JobPersistenceException: Couldn't store trigger
+-- scheduler.scheduleJob(jobDetail, trigger);
 --
 -- 表的结构 `qrtz_cron_triggers`
 --
-
 CREATE TABLE IF NOT EXISTS `qrtz_cron_triggers` (
   `SCHED_NAME` varchar(120) NOT NULL,
   `TRIGGER_NAME` varchar(120) NOT NULL,
